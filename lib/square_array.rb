7cbab1e082384b79ca6ec32fb2313973d06c7463
Vivian Tran <viviantran94@gmail.com>
2019-10-20 01:49:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,15 +6,6 @@ def square_array(array)
   while array[counter] do
     new_array << array[counter]**2
     counter += 1
-    array  << new_array
   end
-retuen new_array
-end
-
-def square_array(numbers)
-  # your code here	  new_numbers = []
-end 	  numbers.each do |num|
-    new_numbers << (num ** 2)
-  end
-  return new_numbers
+  return new_array
 end
