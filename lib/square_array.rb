119bef77ac09223bad954d92a7710df84edab97b
Vivian Tran <viviantran94@gmail.com>
2019-10-20 01:46:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
+
 def square_array(array)
-  # your code here
+  counter = 0
+
+  while array[counter] do
+    new_array = array[counter]**2
+    counter += 1
+    array  << new_array
+  end
+retuen new_array
 end
